09d88fbbf672dd7edc80300f46d3cdc73a679abe
JohnnyBoiTime <74278206+JohnnyBoiTime@users.noreply.github.com>
2024-07-14 21:25:48 -0700

    Update MainAppScreen.js

--- MainAppScreen.js
+++ MainAppScreen.js
@@ -1,42 +1,51 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text} from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useSelector } from 'react-redux';
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
 import Home from './Home';
 import Search from './Search';
 import Saved from './Saved';
 import InsideTheAlbum from './AlbumContents';
-import { NativeBaseProvider, Heading, Box, Button, Input, View, Icon } from 'native-base';
+import MusicPlayer from './MusicPlayer';
+import { NativeBaseProvider,  View } from 'native-base';
 import { LinearGradient } from 'expo-linear-gradient';
 
 
+// Shows tabs at bottom and music player when it appears
+
 const MainAppScreen = () => {
 
-const Tabs = createBottomTabNavigator();
-
-// const currentSong = useSelector((state) => state.currentSong);
+    const Tabs = createBottomTabNavigator();
 
     return (
         <NativeBaseProvider>
-             <LinearGradient
-        colors ={['#800080', '#800080' /*, '#800080'*/]} 
-        locations={[0.1, 1] /* transition */}
-        >
-        </LinearGradient>
-        <NavigationContainer independent={true}>
-            <Tabs.Navigator screenOptions={{
-                headerShown: false, 
-                tabBarStyle: {backgroundColor: 'black'}
+            <LinearGradient
+                colors ={['#800080', '#800080' /*, '#800080'*/]} 
+                locations={[0.1, 1] /* transition */}
+            >
+            </LinearGradient>
+            <NavigationContainer independent={true}>
+                <View style={{flex: 1}}>
+                    { /* Render music player above bar logic thanks to: https://stackoverflow.com/questions/74834004/react-native-how-to-show-an-element-above-bottom-tab-navigator */}
+                <Tabs.Navigator
+                tabBar={(props) => (
+                    <>
+                    <MusicPlayer/>
+                    <BottomTabBar {...props} />
+                    </>
+                )} 
+                screenOptions={{
+                    headerShown: false, 
+                    tabBarStyle: {backgroundColor: 'black'},
                 }}>
-                <Tabs.Screen name ="Home" component={Home} options={{tabBarIcon: () => ( <Ionicons name="md-home" size={38} color="blue"/>) }}/>
-                <Tabs.Screen name ="Search For Music" component={Search} options={{tabBarIcon: () => ( <Ionicons name="search" size={38} color="blue"/>) }}/>
-                <Tabs.Screen name ="Saved Music" component={Saved} options={{tabBarIcon: () => ( <MaterialIcons name="library-music" size={38} color="blue"/>) }}/>
-                <Tabs.Screen name ="AlbumContents" component={InsideTheAlbum} options={{ tabBarButton: () => null}}/>
-            </Tabs.Navigator>
-             </NavigationContainer>
-            </NativeBaseProvider>
+                    <Tabs.Screen name ="Home" component={Home} options={{tabBarIcon: () => ( <Ionicons name="home" size={38} color="blue"/>) }}/>
+                    <Tabs.Screen name ="Search For Music" component={Search} options={{tabBarIcon: () => ( <Ionicons name="search" size={38} color="blue"/>) }}/>
+                    <Tabs.Screen name ="Saved Music" component={Saved} options={{tabBarIcon: () => ( <MaterialIcons name="library-music" size={38} color="blue"/>) }}/>
+                    <Tabs.Screen name ="AlbumContents" component={InsideTheAlbum} options={{ tabBarButton: () => null}}/>
+                </Tabs.Navigator>
+                </View>
+                </NavigationContainer>
+                </NativeBaseProvider>
     );
 };
 
