20ceaa12b3e72011fb705a05273123c6710ff12c
JohnnyBoiTime <74278206+JohnnyBoiTime@users.noreply.github.com>
2024-07-14 21:24:58 -0700

    Add files via upload

---
+++ GenerateAlbum.js
@@ -0,0 +1,109 @@
+// File to generate albums from assets, currently only works with bandcamp
+// format
+
+const fs = require('fs');
+const path = require('path');
+
+// Directory with songs
+const pathToDir = path.join(__dirname, 'assets');
+
+
+
+// Combines file names and song names
+// NOTE: 
+//      Because in windows it sorts files based in alphebetical order,
+//      track list WILL be different from official track list unless 
+//      there is something to differentiate the tracks. 
+async function createAlbumList(directory) {
+    try {
+
+        // Variables for specific parts of directories/files
+        const files = await fs.promises.readdir(directory);
+        let albumNames = [];
+        let listOfAlbums = [];
+        
+        for (const file of files) {
+            const filePath = path.join(directory, file);
+            const stats = await fs.promises.stat(filePath); // Creates path to file
+            
+            // If a folder is found, iterate through the songs and retrieve info like
+            // artist name, song names, the file paths, album cover
+            if (stats.isDirectory()) {
+                albumNames.push(file);
+                const singleAlbumName = file 
+                const pathToInsideDir = path.join('assets', `${file}`);
+                const insideDir = await fs.promises.readdir(pathToInsideDir);
+                
+                let songPaths = []; 
+                let songNames = []; 
+                let albumCover = ''; 
+                let artistName = '';
+                let songName = '';
+
+                // Now inside folder, so get songs
+                for (const fileInsideDir of insideDir) {
+
+                    // In bandcamp format, first file is always the cover
+                    if (fileInsideDir.toLowerCase().includes('cover')) {
+                        albumCover = `require('./assets/${file}/${fileInsideDir}')`;
+                    }
+                    // After cover, get songs, split into the actual path and song name string 
+                    else {
+                        songPaths.push(`require('./assets/${file}/${fileInsideDir}')`);
+                        songName = getSongName(fileInsideDir);
+                        songNames.push(songName);
+                        artistName = fileInsideDir.split('-');
+                        
+                    }
+                }
+                
+                // Format album
+                listOfAlbums.push({
+                    title: singleAlbumName,
+                    cover: albumCover,
+                    artist: artistName[0],
+                    songs: {
+                        title: songNames,
+                        path: songPaths,
+                    }
+                });
+            }
+        }
+
+        // .replace(/"require\(.*?)\)"/g, 'require($1)'):
+        //  finds every instance of "require" globally (/g), captures what is
+        //  in the parantheses \((.*?)\), then replaces it with require method with
+        //  what was captured being put inside of the parantheses ($1 holds the capture)
+        //  https://regex101.com/r/iQ5gyQ/1
+        //  https://stackoverflow.com/questions/13247864/javascript-replace-string-between-brackets-but-the-brackets-should-stay
+        const outputAlbumList = `export const importedAlbums = ${JSON.stringify(listOfAlbums, null, 1).replace(/"require\((.*?)\)"/g, 'require($1)')};`;
+        
+        // Output results
+        fs.writeFile('Albums.js', outputAlbumList, error => {
+            if (error) {
+                console.log(`Could not import albums: ${error}`);
+            }
+            console.log("Successfully wrote to file!");
+        });
+
+    } catch (error) {
+        throw new Error(`Could not read directory: ${error}`);
+    }
+};
+
+// Extract just the song name 
+function getSongName(file) {
+    let songNameNoMP3 = file.replace('.mp3', ''); // Get rid of .mp3
+    let components = songNameNoMP3.split(' - '); // Section out song title between the -
+    let songNameWithNum = components[components.length - 1]; // Focus on the track number with song name part
+    let songName = songNameWithNum.slice(songNameWithNum.indexOf(' ') + 1); // Slice from after space to end
+    return songName; // Return just song name
+
+
+}
+
+createAlbumList(pathToDir);
+
+
+
+
---
+++ MusicPlayerContext.js
@@ -0,0 +1,185 @@
+import React, { createContext, Component, useState, setState, useRef, useEffect, useContext } from "react";
+import { Audio } from "expo-av";
+import { current } from "@reduxjs/toolkit";
+import * as FileSystem from 'expo-file-system';
+
+
+// Context screen to control variables 
+
+const MusicPlayerContext = createContext();
+
+export const MusicPlayerProvider = ({ children }) => {
+    const [nameOfSong, setNameOfSong] = useState('');
+    const [nameOfAlbum, setNameOfAlbum] = useState('');
+    const [albumCover, setAlbumCover] = useState('');
+    const [nameOfArtist, setNameOfArtist] = useState('');
+    const [nameOfSongMP3, setNameOfSongMP3] = useState('');
+    const [currentSongList, setCurrentSongList] = useState({ title: '', path: ''});
+    const [songIndex, setSongIndex] = useState(0);
+    const [nextSong, setNextSong] = useState('');
+    const [prevSong, setPrevSong] = useState('');
+    const [colorOfSong, setColorOfSong] = useState('white');
+    const [album, setAlbum] = useState([{}]);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [playbackPos, setPlaybackPos] = useState(0);
+    const [playbackDuration, setPlaybackDuration] = useState(0);
+    const [numSongs, setNumSongs] = useState(0);
+    const songRef = useRef(null);
+
+
+    // Ensures no song is playing
+    useEffect(() => {
+        if (songRef.current) {
+            //   song.stopAsync();
+            //   song.unloadAsync();
+               songRef.current.stopAsync();
+               songRef.current.unloadAsync();
+           }
+    }, []);
+
+     // Plays song
+     const resumeSong = async () => {
+        try{
+            await songRef.current.getStatusAsync();
+            if (songRef.current) {
+                await songRef.current.playAsync();
+                setIsPlaying(true)
+            }
+        } catch (error) {
+            console.log("Song could not be played", error);
+        }
+    };
+
+    // Function inspired by Kartikey from this post: https://stackoverflow.com/questions/68042313/pausing-react-native-expo-audio
+    const pauseSong = async () => {
+        try {
+            const status = await songRef.current.getStatusAsync();
+            if (status.isLoaded && status.isPlaying) {
+                    await songRef.current.pauseAsync();
+                    setIsPlaying(false);
+            }
+        } catch (error) {
+            console.log("Song could not be paused", error);
+
+        }
+    };
+
+    // Skip to next song
+    const skipForward = () => {
+        const nextIndex = (songIndex + 1) % numSongs;
+         setSongIndex(nextIndex);
+    }
+
+    // Go back to previous song
+    const goBack = () => {
+        console.log(songIndex);
+        const prevIndex = (songIndex - 1 + numSongs) % numSongs;
+        setSongIndex(prevIndex);
+    }
+
+    // Position of scroller
+    const scroller = async (value) => {
+        if (songRef.current) {
+            const positionBall = value * playbackDuration;
+            await songRef.current.setPositionAsync(positionBall);
+            setPlaybackPos(position);
+        }
+    };
+
+    const time = (millis) => {
+        const minutes = Math.floor(millis / 60000);
+        const seconds = Math.floor((millis % 60000) / 1000);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+
+    useEffect(() => {
+        const startSong = async () => {
+
+            // When song is tapped again, restarts song
+            // When a different song is played, unloads previous song
+            // And loads in new one
+            if (songRef.current) {
+                await songRef.current.stopAsync();
+                await songRef.current.unloadAsync();
+            }
+    
+            // Creates/plays sound
+            const {sound: newSound} = await Audio.Sound.createAsync(currentSongList.path[songIndex]);
+            songRef.current = newSound;
+            await newSound.playAsync();
+            setIsPlaying(true);
+            console.log(songRef.current);
+            setNameOfSong(currentSongList.title[songIndex]);
+
+            // Updates current positon of the song (in milliseconds)
+            newSound.setOnPlaybackStatusUpdate((status) => {
+                if (status.isLoaded) {
+                    setPlaybackPos(status.positionMillis);
+                    setPlaybackDuration(status.durationMillis);
+                    if (status.didJustFinish) {
+                        setIsPlaying(false);
+                        const nextIndex = (index + 1) % numSongs;
+                        setSongIndex(nextIndex);
+                        startSong(nextIndex);
+                    }
+                }
+            });
+
+            console.log("song be playing!");
+        };
+        startSong();
+    }, [songIndex]);
+
+    return (
+        <MusicPlayerContext.Provider
+            value={{
+                currentSongList,
+                nameOfSong,
+                nameOfAlbum,
+                nameOfArtist,
+                songIndex,
+                isPlaying,
+                playbackPos,
+                playbackDuration,
+                songRef,
+                album,
+                numSongs,
+                nameOfSongMP3,
+                albumCover,
+                nextSong,
+                prevSong,
+                colorOfSong,
+                time,
+                resumeSong,
+                pauseSong,
+                saveSong,
+                skipForward,
+                goBack,
+                scroller,
+                setColorOfSong,
+                setNextSong,
+                setPrevSong,
+                setNumSongs,
+                setAlbum,
+                setCurrentSongList,
+                setNameOfSong,
+                setNameOfAlbum,
+                setNameOfArtist,
+                setNameOfSongMP3,
+                setSongIndex,
+                setIsPlaying,
+                setPlaybackPos,
+                setPlaybackDuration,
+                setAlbumCover,
+            }}
+        >
+            {children}
+        </MusicPlayerContext.Provider>
+    );
+};
+
+export const useMusicPlayer = () => {
+    return useContext(MusicPlayerContext);
+};
+
