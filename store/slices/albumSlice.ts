44ee5071b26310d3c2fead0dc63f9b5a2b4bb1d8
JohnnyBoiTime <74278206+JohnnyBoiTime@users.noreply.github.com>
2024-07-26 17:46:47 -0700

    Create albumSlice.ts

---
+++ store/slices/albumSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// Album info needs:
+// song name
+// name of album
+// name of artist
+// album cover
+// number of songs
+
+// Album structure
+interface AlbumState {
+    nameOfSong: string;
+    nameOfAlbum: string;
+    nameOfArtist: string;
+    albumCover: number;
+    numSongs: number;
+}
+
+// Set initial states
+const initialState: AlbumState = {
+    nameOfSong: '',
+    nameOfAlbum: '',
+    nameOfArtist: '',
+    albumCover: 8965,
+    numSongs: 0,
+};
+
+// Reducers
+const albumSlice = createSlice({
+    name: 'album',
+    initialState,
+    reducers: {
+        setNameOfSong(state, action: PayloadAction<string>) { 
+            state.nameOfSong = action.payload;
+        },
+        setNameOfAlbum(state, action: PayloadAction<string>) {
+            state.nameOfAlbum = action.payload;
+        },
+        setNameOfArtist(state, action: PayloadAction<string>) {
+            state.nameOfArtist = action.payload;
+        },
+        setAlbumCover(state, action: PayloadAction<number>) {
+            state.albumCover = action.payload;
+        },
+        setNumSongs(state, action: PayloadAction<number>) {
+            state.numSongs = action.payload;
+        },
+    },
+});
+
+// exports actions to use in app
+export const {setNameOfSong, setNameOfAlbum, setNameOfArtist, setAlbumCover, setNumSongs} = albumSlice.actions; 
+export default albumSlice.reducer; // exports reducer to handle actions
