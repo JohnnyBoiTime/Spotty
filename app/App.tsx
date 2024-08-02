49a9dab6cb01d0054b00bee423ea4e89293978dd
JohnnyBoiTime <74278206+JohnnyBoiTime@users.noreply.github.com>
2024-08-01 19:45:30 -0700

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -2,12 +2,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { AudioProvider } from "./context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import LogIn from './LogIn';
 import SignUp from "./signUp";
 import ForgotPassword from "./forgotPassword";
 import store from '../store/index';
 import MainAppScreen from "./tabs/mainAppScreen";
-
 
 const Stack = createStackNavigator();
 
@@ -15,19 +15,21 @@ const App = () => {
   return (
     // Provides store to app
     <Provider store={store}> 
-      <AudioProvider>
-        <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}>
-          <Stack.Screen name="login" component={LogIn}/>
-          <Stack.Screen name="home" component={MainAppScreen} />
-          <Stack.Screen name="signUp" component={SignUp}/>
-          <Stack.Screen name="forgotPassword" component={ForgotPassword} />
-        </Stack.Navigator>
-        </NavigationContainer>
-      </AudioProvider>
+      <GestureHandlerRootView>
+        <AudioProvider>
+          <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}>
+            <Stack.Screen name="login" component={LogIn}/>
+            <Stack.Screen name="home" component={MainAppScreen} />
+            <Stack.Screen name="signUp" component={SignUp}/>
+            <Stack.Screen name="forgotPassword" component={ForgotPassword} />
+          </Stack.Navigator>
+          </NavigationContainer>
+        </AudioProvider>
+      </GestureHandlerRootView>
     </Provider>
   );
 }
