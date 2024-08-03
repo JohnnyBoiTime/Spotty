6f0a07574486f59389d67de5375327d90780c230
Calvin Wieland <66046869+CalvinWieland@users.noreply.github.com>
2024-08-03 15:33:47 -0700

    Add more comments to new upload and delete

--- EC2Files/s3Utils.js
+++ EC2Files/s3Utils.js
@@ -27,10 +27,12 @@ const getPresignedUrlForUpload = async ( bucketName, key, expiresIn = 3600) => {
         const command = new PutObjectCommand({ Bucket: bucketName, Key: key, ContentType: 'application/octet-stream' });
 
         try {
+                //get signed URL from amazon
                 const url = await getSignedUrl(client, command, { expiresIn: 3600 });
                 return url;
         }
         catch(err) {
+                //throw error and print to console
                 console.error('Couldn\'t generate presigned URL', err);
                 throw err;
         }
@@ -40,9 +42,11 @@ const deleteFile = async (bucketParams) => {
         //Folowing code from https://docs.aws.amazon.com/AmazonS3/latest/userguide/delete-objects.html
 
         try {
+                //attempt to delete object and store returned data
                 const data = await client.send(new DeleteObjectCommand(bucketParams));
                 return data;
         } catch (err) {
+                //print error to console
                 console.error("Error: ", err);
                 throw err;
         }
